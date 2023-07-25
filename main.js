cc495055a467be467a6ed5103a69d80d0a473d31
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2023-07-25 20:51:29 +0500

    added javascript and some style

---
+++ main.js
@@ -0,0 +1,136 @@
+const question = [{
+        // Q1
+        question: "Which is largest animal in the world?",
+        answers: [{
+                text: "Shark",
+                correct: false
+            },
+            {
+                text: "Blue Whale",
+                correct: true
+            },
+            {
+                text: "Elephant",
+                correct: false
+            },
+            {
+                text: "Giraffe",
+                correct: false
+            },
+        ]
+    },
+    {
+        // Q2
+        question: "Which is the smallest country in the world?",
+        answers: [{
+                text: "Vatican City",
+                correct: true
+            },
+            {
+                text: "Bhutan",
+                correct: false
+            },
+            {
+                text: "Nepal",
+                correct: false
+            },
+            {
+                text: "Sri Lanka",
+                correct: false
+            },
+        ]
+    },
+    {
+        // Q3
+        question: "Whtch is the largest desert in the world?",
+        answers: [{
+                text: "Sahara",
+                correct: true
+            },
+            {
+                text: "Cholistan",
+                correct: false
+            },
+            {
+                text: "Thar",
+                correct: false
+            },
+            {
+                text: "Antarctica",
+                correct: false
+            },
+        ]
+    },
+    {
+        // Q4
+        question: "Which is smallest content in the world?",
+        answers: [{
+                text: "Asia",
+                correct: false
+            },
+            {
+                text: "Australia",
+                correct: true
+            },
+            {
+                text: "Urope",
+                correct: false
+            },
+            {
+                text: "Africa",
+                correct: false
+            },
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("ans-btn");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion() {
+    resetState();
+    let currentQuestion = question[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState() {
+    nextButton.style.display = "none";
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect) {
+        selectedBtn.classList.add("correct");
+    } else {
+        selectedBtn.classList.add("incorrect");
+    }
+}
+
+startQuiz();
